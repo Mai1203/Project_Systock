70afba12696eeb8fb2d4f2fac990cb5056cfc671
Mai1203 <83738249+Mai1203@users.noreply.github.com>
2025-05-13 17:10:01 -0500

    Obtener y mostrar los clientes de la base de datos

--- app/view/ClientesView.py
+++ app/view/ClientesView.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QMessageBox
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtCore import Qt, QTimer
+
 from ..ui import Ui_ControlCliente
 from ..database.database import SessionLocal
 from ..controllers.clientes_crud import *
@@ -36,6 +37,11 @@ class Cliente_View(QWidget, Ui_ControlCliente):
             self.InputDireccion
         ]
 
+    def showEvent(self, event):
+        super().showEvent(event)
+        self.limpiar_campos()
+        self.mostrar_clientes()
+
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Down:
             self.navegar_anterior()
@@ -57,3 +63,56 @@ class Cliente_View(QWidget, Ui_ControlCliente):
             index = self.campos.index(actual)
             anterior = self.campos[(index - 1) % len(self.campos)]
             anterior.setFocus()
+
+    def limpiar_campos(self):
+        self.InputApellido.clear()
+        self.InputCedula.clear()
+        self.InputDireccion.clear()
+        self.InputNombre.clear()
+        self.InputTelefono.clear()
+
+    def mostrar_clientes(self):
+        self.db = SessionLocal()
+        rows = obtener_clientes(self.db)
+
+        self.actualizar_tabla_ventasCredito(rows)
+
+        self.db.close()
+
+    def actualizar_tabla_ventasCredito(self, rows):
+        if not rows:
+            print("No hay filas para mostrar.")
+            self.TablaClientes.setRowCount(0)
+            return
+
+        try:
+            self.TablaClientes.setRowCount(0)
+
+            rows.sort(key=lambda x: x.ID_Cliente, reverse=False)
+            
+            for row_idx, row in enumerate(rows):
+                
+                id_cliente = str(row.ID_Cliente)
+                nombre = str(row.Nombre)
+                apellido = str(row.Apellido)
+                direccion = str(row.Direccion)
+                telefono = str(row.Teléfono)
+
+                self.TablaClientes.insertRow(0)
+                
+                # Configurar items de la tabla
+                items = [
+                    (id_cliente, 0),
+                    (nombre, 1),
+                    (apellido, 2),
+                    (telefono, 3),
+                    (direccion, 4)
+                ]
+
+                # Añadir items a la tabla
+                for value, col_idx in items:
+                    item = QtWidgets.QTableWidgetItem(value)
+                    item.setTextAlignment(QtCore.Qt.AlignCenter)
+                    self.TablaClientes.setItem(0, col_idx, item)
+        except Exception as e:
+            print(f"Error al mostrar tabla clientes: {e}")
